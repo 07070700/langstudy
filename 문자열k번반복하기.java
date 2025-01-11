24a6d32957debbe8ae1a6c8cfb40256307c0fd47
unknown <2012230@naver.com>
2025-01-11 15:24:28 +0900

    java

---
+++ 문자열k번반복하기.java
@@ -0,0 +1,24 @@
+// 문자열 k번 반복하기기
+class Solution {
+    public String solution(String my_string, int k) {
+        
+        String answer= my_string;
+        int i=1;
+        // string 을 붙여서 answer 문자열 재선언이 되어버려서 안됨. 
+        // 자바에서는 문자열 재선언이 안됨
+        while (i<k) {
+        answer += my_string;
+        i++;
+        }
+        
+        return answer;
+    }
+}
+
+// 좋은 답안
+
+class Solution2 {
+    public String solution(String my_string, int k) {
+        return my_string.repeat(k);
+    }
+}
---
+++ 짝수홀수.java
@@ -0,0 +1,33 @@
+
+// 입력값이 짝수일때 홀수일때 다르게 출력하기기
+// 3항연산자를 이용한 방법
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.print(n + " is "+(n % 2 == 0 ? "even" : "odd"));
+
+
+    }
+}
+
+
+// 내 풀이 (불리안 이용용)
+
+import java.util.Scanner;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean bool= n % 2 == 0;
+        if (bool) {
+            System.out.println(n + " is even");
+        }else{
+        System.out.println(n + " is odd");
+            }
+    }
+}
